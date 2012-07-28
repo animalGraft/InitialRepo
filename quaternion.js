e12bb47c005cec17b8ad27fcf67bba6c82c902bf
animalGraft <blazor@ramone.com>
2012-07-28 00:41:25 -0400

    Continued to add to quaternion file
    For now this will still be "funciton" instead of fully OOP.

--- quaternion.js
+++ quaternion.js
@@ -1,28 +1,39 @@
-quaternion = {};
-quaternion.create = function(inQuat)
-{
-	var result = new Float32Array(4);
+
+var quaternion = {};
+quaternion.create = function () {
+	var result = [];
 	result[0] = result[1] = result[2] = 0;
 	result[3] = 1;
 	return result;
-}
+};
 
-quaternion.multiply = function(op1, op2)
-{
-	var result = new Float32Array(4);
-//var result.w = op1.w * op2.w 	- op1.x * op2.x 	- op1.y * op2.y 	- op1.z * op2.z;
-//var result.x = op1.w * op2.x 	+ op1.x * op2.w 	+ op1.y * op2.z 	- op1.z * op2.y;
-//var result.y = op1.w * op2.y	- op1.x * op2.z 	+ op1.y * op2.w 	+ op1.z * op2.x;
-//var result.y = op1.w * op2.z 	+ op1.x * op2.y 	- op1.y * op2.x 	+ op1.z * op2.w;
-	
-	var result[3] = op1[3] * op2[3] 	- op1[0] * op2[0] 	- op1[1] * op2[1] 	- op1[2] * op2[2];
-	var result[0] = op1[3] * op2[0] 	+ op1[0] * op2[3] 	+ op1[1] * op2[2] 	- op1[2] * op2[1];
-	var result[1] = op1[3] * op2[1]		- op1[0] * op2[2] 	+ op1[1] * op2[3] 	+ op1[2] * op2[0];
-	var result[1] = op1[3] * op2[2] 	+ op1[0] * op2[1] 	- op1[1] * op2[0] 	+ op1[2] * op2[3];
-	
+quaternion.multiply = function (op1, op2) {
+	var result = [];
+	result[3] = op1[3] * op2[3] - op1[0] * op2[0] - op1[1] * op2[1] - op1[2] * op2[2];
+	result[0] = op1[3] * op2[0] + op1[0] * op2[3] + op1[1] * op2[2] - op1[2] * op2[1];
+	result[1] = op1[3] * op2[1] - op1[0] * op2[2] + op1[1] * op2[3] + op1[2] * op2[0];
+	result[2] = op1[3] * op2[2] + op1[0] * op2[1] - op1[1] * op2[0] + op1[2] * op2[3];
 	return result;
-}
+};
 
+quaternion.conjugate = function (op) {
+	var result = [];
+	result[0] = -op[0];
+	result[1] = -op[1];
+	result[2] = -op[2];
+	result[3] = op[3];
+};
+
+quaternion.rotateVector = function (quat, vec) {
+	//check for proper inputs?
+	var vecAsQuat = [];
+	vecAsQuat[0] = vec[0];
+	vecAsQuat[1] = vec[1];
+	vecAsQuat[2] = vec[2];
+	vecAsQuat[3] = 0;
+
+	return quaternion.multiply(quaternion.multiply(quat, vecAsQuat), quaternion.conjugate(quat));
+};
 //from angle axis
 //get angle axis
 
