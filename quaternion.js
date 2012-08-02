65d80193d405539b76b1d51a6e6853686cb130b7
animalGraft <blazor@ramone.com>
2012-08-01 23:23:10 -0400

    Added a to matrix and to string function
    various changes to the testing app

--- quaternion.js
+++ quaternion.js
@@ -42,6 +42,28 @@ quaternion.fromAngleAxis = function (angle, axis) {
 	return [sinAngle * normalAxis[0], sinAngle * normalAxis[1], sinAngle * normalAxis[2], Math.cos(angleRads * 0.5)];
 };
 
+quaternion.toMatrix = function (quat) {
+	var wx2 = quat[3] * quat[0] * 2;
+	var wy2 = quat[3] * quat[1] * 2;
+	var wz2 = quat[3] * quat[2] * 2;
+	var xy2 = quat[0] * quat[1] * 2;
+	var xz2 = quat[0] * quat[2] * 2;
+	var yz2 = quat[1] * quat[2] * 2;
+	var xx2 = quat[0] * quat[0] * 2;
+	var yy2 = quat[1] * quat[1] * 2;
+	var zz2 = quat[2] * quat[2] * 2;
+	var mat = [];
+	mat[0] = 1 - yy2 - zz2; mat[1] = xy2 + wz2; mat[2] = xz2 - wy2;
+	mat[3] = xy2 - wz2; mat[4] = 1 - xx2 - zz2; mat[5] = yz2 + wx2;
+	mat[6] = xz2 + wy2; mat[7] = yz2 - wx2; mat[8] = 1 - xx2 - yy2;
+	
+	return mat;
+}
+
+quaternion.toString = function(quat) {
+	return "w=" + quat[3] + ", x=" + quat[0] + ", y=" + quat[1] + ", z=" + quat[2];  
+}
+
 var normalize = function (axis) {
 	var len = Math.sqrt(axis[0] * axis[0] +  axis[1] * axis[1] +  axis[2] * axis[2]);
 	return [ axis[0] / len, axis[1] / len, axis[2] / len];
